35bdad92b7ef2b459d3cce2755da14c532773f42
Riki Fujihara <rikifujihara@gmail.com>
2024-04-06 17:17:26 +1100

    add and get workouts

---
+++ Models/Workout.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace workoutsbackend.Models;
+
+public class Workout
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [Required]
+    public string? Title { get; set; }
+
+    [Required]
+    public int Reps { get; set; }
+
+    [Required]
+    public int Load { get; set; }
+}
---
+++ Services/MongoDBService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using workoutsbackend.Models;
+
+namespace workoutsbackend.Services;
+
+public class MongoDBService
+{
+    private readonly IMongoCollection<Workout> _workoutsCollection;
+
+    public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+    {
+        MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+        IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+        _workoutsCollection = database.GetCollection<Workout>(mongoDBSettings.Value.CollectionName);
+    }
+
+    public async Task<List<Workout>> GetAsync()
+    {
+        return await _workoutsCollection.Find(new BsonDocument()).ToListAsync();
+    }
+
+    public async Task CreateAsync(Workout workout)
+    {
+        await _workoutsCollection.InsertOneAsync(workout);
+        return;
+    }
+
+
+}
